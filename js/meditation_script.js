c7dbcf5af37abdc2ddebd03253b39522feacd670
Jon Guerrera <jrrera@gmail.com>
2014-03-23 14:34:39 -0700

    Adds goal setting functionality and improves commenting

--- js/meditation_script.js
+++ js/meditation_script.js
@@ -5,14 +5,12 @@ app.filter('minutes', function(){
 		var minutes = Math.floor(val / 60),
 			secondsRemainder = parseInt(val % 60);
 
-		console.log('minutes:', minutes, 'secondsRemainder:', secondsRemainder);
-
 		if (secondsRemainder && minutes) {
-			return minutes + ' minutes and ' + secondsRemainder + ' seconds.' 
+			return minutes + ' minutes and ' + secondsRemainder + ' seconds';
 		} else if (minutes) {
-			return minutes + ' minutes'
+			return minutes + ' minutes';
 		} else {
-			return secondsRemainder + ' seconds'
+			return secondsRemainder + ' seconds';
 		}
 	}
 });
@@ -22,7 +20,7 @@ app.factory('DataService', function(){
 	// This API will eventually move to AppEngine
 	return {
 		saveSession: function(session) {
-			var sessions = JSON.parse(localStorage['meditationData'])
+			var sessions = JSON.parse(localStorage['meditationData']);
 			sessions.push(session);
 
 			localStorage['meditationData'] = JSON.stringify(sessions);
@@ -36,10 +34,20 @@ app.factory('DataService', function(){
 			}
 		},
 		deleteSession: function(session, index) {
-			var sessions = JSON.parse(localStorage['meditationData'])
+			var sessions = JSON.parse(localStorage['meditationData']);
 			sessions.splice(index, 1);
 
 			localStorage['meditationData'] = JSON.stringify(sessions);
+		},
+
+		saveGoal: function(duration) {
+			localStorage['meditationGoal'] = duration;
+		}, 
+
+		getGoal: function() {
+			var duration = parseInt(localStorage['meditationGoal']);
+			// Return 540 seconds by default if no valid data to pull
+			return duration ? duration : 600;
 		}
 	};
 });
@@ -48,44 +56,95 @@ app.controller('ClockCtrl', ['$scope', '$timeout', 'DataService', function($scop
 	$scope.begin = false;
 	$scope.result = "";
 
-	$scope.pastActivity = DataService.getSessions();
-	
-	console.log('pastActivity is', $scope.pastActivity)
+	// Default values for changing your meditation goal
+	$scope.goalChange = {
+		minutes: 0,
+		seconds: 0
+	};
 
-	// Converts minutes to seconds
+	// Seconds per day of meditation that is your goal
+	// This will eventually be part of onboarding onto the app
+	$scope.goal = DataService.getGoal(); 
+
+	// Get past activity
+	$scope.pastActivity = DataService.getSessions();
+
+	/**
+	 * Converts minutes to seconds for displaying in the view
+	 * @param {integer} minutes The amount of minutes to convert to seconds
+	 */
 	$scope.convertDurationToSeconds = function(minutes) {
 		console.log('lets convert');
 		$scope.duration = isNaN(minutes) ? 0 : (minutes * 60);
 	};
 
-	// This function is meant to be executed by the timer clock at completion
-	// @param - duration {int}
+	/**
+	 * This function is executed by the timer clock at completion
+	 * @param {integer} duration The duration of the session in seconds
+	 */
 	$scope.trackTime = function(duration) {
 
-		var session = {
+		var reachedGoal, sessionData;
+
+		// First, check to see if we met our goal for the day
+		reachedGoal = (duration > $scope.goal ? true : false); 
+
+		console.log('Did you reach your goal?', reachedGoal);
+
+		// Create the session object
+		sessionData = {
 			date: new Date().getTime(),
-			duration: duration
+			duration: duration,
+			goal: $scope.goal,
+			reachedGoal: reachedGoal
 		}; 
 
+		// Announce your accomplishment
 		$scope.result = 'Congrats! You meditated for ' + duration + ' seconds!'
 		
+		// Remove the results message after 5 seconds
 		$timeout(function(){
 			$scope.result = "";
 		}, 5000);
 
-
-		$scope.pastActivity.push(session);
-
-		DataService.saveSession(session);
+		// Save the data on model and in db
+		$scope.pastActivity.push(sessionData);
+		DataService.saveSession(sessionData);
 	};
 
+	/**
+	 * Deletes are particular session when the 'Delete' button is clicked
+	 * @param {object} session The session you're deleting
+	 * @param {integer} index The position of this session in the sessions array
+	 * @returns {boolean} Returns true if successful
+	 */	
 	$scope.deleteSession = function(session, index) {
 		if (confirm('Are you sure? There\'s no undoing this action.')) {
 			$scope.pastActivity.splice(index,1);
 			DataService.deleteSession(session, index);	
 		}
-
+		return true;
 	};
+
+	/**
+	 * Changes your goal time
+	 * @param {integer} minutes The number of minutes of the new goal
+	 * @param {integer} seconds Number of seconds of the new goal
+	 * @returns {boolean} Returns true if successful
+	 */	
+	$scope.changeGoal = function(minutes, seconds) {
+		// If either isn't a number, return false immediately
+		if (isNaN(minutes) || isNaN(seconds)) return false;
+		
+
+		// Convert both values to total seconds
+		$scope.goal = parseInt(seconds) + parseInt(minutes * 60);
+		
+		// Save the data and hide the goal changer menu
+		DataService.saveGoal($scope.goal);
+		$scope.showGoalChanger = false; //Reset display of goal changer
+		return true;
+	}
 }]);
 
 /**
@@ -144,7 +203,7 @@ app.directive('clock', function(){
 					});
 				}
 			});
-		}
-	}
-})
+		} // end link function
+	};
+});
 
